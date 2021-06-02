3d0d0c27fbd983d8c30dd952c97364489112711b
SpdDev <spd@tutanota.com>
2021-06-02 16:29:35 +0400

    First commit

--- lesson1/Lesson1.py
+++ lesson1/Lesson1.py
@@ -1,7 +1,7 @@
 #pip install beautifulsoup4
 from bs4 import BeautifulSoup
 
-with open("blank/index.html") as file:
+with open("blank/index2.html", encoding="utf-8") as file:
     src = file.read()
 # print(src)
 
@@ -25,5 +25,52 @@ title = soup.title
 # user_name = soup.find("div", class_="user__name")
 # print(user_name.text.strip())
 
-user_name = soup.find("div", {"class": "user__name"}).find("span").text
-print(user_name)
+# user_name = soup.find("div", {"class": "user__name"}).find("span").text
+# print(user_name)
+
+# user_name = soup.find("div", {"class": "user__name", "id" : "aaa"}).find("span").text
+# print(user_name)
+
+# find_all_spans_in_user_info = soup.find(class_="user__info").find_all("span")
+# print(find_all_spans_in_user_info)
+
+# for item in find_all_spans_in_user_info:
+    # print(item.text)
+
+# print(find_all_spans_in_user_info[0])
+# print(find_all_spans_in_user_info[2].text)
+
+# social_links = soup.find(class_="social__networks").find(
+#     "ul").find_all("a")
+# print(social_links)
+
+# all_a = soup.find_all("a")
+# print(all_a)
+
+# for item in all_a:
+#     item_text = item.text
+#     item_url = item.get("href")
+#     # print(item_url)
+#     print(f"{item_text} : {item_url}")
+
+# .find_parent()  .find_parents()
+
+# post_div = soup.find(class_="post__text").find_parent("div", "user__post")
+# print(post_div)
+
+# post_divs = soup.find(class_="post__text").findParents("div", "user__post")
+# print(post_divs)
+
+# .next_element   .previous_element
+# next_el = soup.find(class_="post__title").next_element.next_element.text
+# print(next_el)
+
+# next_el = soup.find(class_="post__title").find_next().text
+# print(next_el)
+
+# .find_next_sibling()  .find_previous_sibling()
+# next_sib = soup.find(class_="post__title").find_next_sibling()
+# print(next_sib)
+
+prev_sib = soup.find(class_="post__date").find_previous_sibling()
+print(prev_sib)
