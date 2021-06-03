df77a526fe7643527ae55b91900c8a19d3afab78
SpdDev <spd@tutanota.com>
2021-06-03 15:31:15 +0400

    First commit

--- lesson2/lesson2.py
+++ lesson2/lesson2.py
@@ -3,13 +3,30 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = "http://www.health-diet.ru/table_calorie"
+# url = "http://www.health-diet.ru/table_calorie"
+#
+# headers = {
+#     "Accept" : "*/*",
+#     "User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:88.0) Gecko/20100101 Firefox/88.0"
+# }
+#
+# req = requests.get(url, headers=headers)
+# src = req.text
+# # print(src)
+#
+# with open("index.html", "w", encoding="utf-8") as file:
+#     file.write(src)
 
-headers = {
-    "Accept" : "*/*",
-    "User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:88.0) Gecko/20100101 Firefox/88.0"
-}
+with open("index.html", encoding="utf-8") as file:
+    src = file.read()
 
-req = requests.get(url, headers=headers)
-src = req.text
-print(src)
+soup = BeautifulSoup(src, "lxml")
+all_categories_dict = {}
+all_products_hrefs = soup.find_all(class_="mzr-tc-group-item-href")
+for item in all_products_hrefs:
+    item_text = item.text
+    item_href = "http://www.health-diet.ru" + item.get("href")
+    # print(f"{item_text}:{item_href}")
+
+    all_categories_dict[item_text] = item_href
+    # print(all_categories_dict)
